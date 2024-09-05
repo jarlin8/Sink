e5744b1227187b25bf26ce654c270efc2b3cff39
Arielante Lee <81870669+jarlin8@users.noreply.github.com>
2024-09-05 12:36:25 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  plugins: [  
-    { src: '~/plugins/matomo.js', mode: 'client' }  
-  ], 
   devtools: { enabled: true },
 
   modules: [
