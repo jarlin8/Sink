f4888bbc77a0ea34326e1a2ee1efaaaf4c9aff7b
Arielante Lee <81870669+jarlin8@users.noreply.github.com>
2024-09-05 12:17:00 +0800

    Create matomo.js
    new track for slink.

---
+++ matomo.js
@@ -0,0 +1,20 @@
+<!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDomains", ["*.ssgg.net","*.ifttt.fun"]]);
+  _paq.push(["enableCrossDomainLinking"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://mato.ifttt.fun/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '9']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+<!-- Matomo Image Tracker-->
+<img referrerpolicy="no-referrer-when-downgrade" src="https://mato.ifttt.fun/matomo.php?idsite=9&amp;rec=1" style="border:0" alt="" />
+<!-- End Matomo -->
